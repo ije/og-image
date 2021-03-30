0397cdb2023e01c53377a5d6f8dd75f76ca7c105
Je Xia <xiawenjie.798@bytedance.com>
2021-03-30 22:41:46 +0800

    feat: git init

---
+++ README.md
@@ -0,0 +1,29 @@
+# [Open Graph Image as a Service](https://og-image.vercel.app)
+
+<a href="https://twitter.com/vercel">
+    <img align="right" src="https://og-image.vercel.app/tweet.png" height="300" />
+</a>
+
+Serverless service that generates dynamic Open Graph images that you can embed in your `<meta>` tags.
+
+For each keystroke, headless chromium is used to render an HTML page and take a screenshot of the result which gets cached.
+
+See the image embedded in the tweet for a real use case.
+
+
+## What is an Open Graph Image?
+
+Have you ever posted a hyperlink to Twitter, Facebook, or Slack and seen an image popup?
+How did your social network know how to "unfurl" the URL and get an image?
+The answer is in your `<head>`.
+
+The [Open Graph protocol](http://ogp.me) says you can put a `<meta>` tag in the `<head>` of a webpage to define this image.
+
+It looks like the following:
+
+```html
+<head>
+  <title>Title</title>
+  <meta property="og:image" content="http://example.com/logo.jpg" />
+</head>
+```
---
+++ og-image.ts
@@ -0,0 +1,78 @@
+import Canvas, { loadImage } from 'https://deno.land/x/canvas@v1.1.1/mod.ts'
+
+addEventListener('fetch', async (event: any) => {
+    const canvasSize = [2048, 1170]
+    const defaultHeight = 300
+    const defaultFontSize = 90
+    const spacing = 200
+
+    const canvas = Canvas.MakeCanvas(canvasSize[0], canvasSize[1])
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = '#ffffff'
+    ctx.fillRect(0, 0, 2048, 1170)
+
+    // draw bg dots
+    ctx.fillStyle = '#cccccc'
+    const g = 100
+    for (let x = 0; x < Math.ceil(canvasSize[0] / g); x++) {
+        for (let y = 0; y < 2 * Math.ceil(canvasSize[1] / g); y++) {
+            let offsetX = 0
+            if (y % 2 === 0) {
+                offsetX = g / 2
+            }
+            ctx.fillRect(g / 2 + x * g + offsetX, g / 2 + y * g / 2, 4, 4)
+        }
+    }
+
+    const url = new URL(event.request.url)
+    const text = decodeURIComponent(url.pathname).slice(1)
+    const imageSrc = url.searchParams.get('image') || url.searchParams.get('img') || 'https://deno.com/static/logo3.png'
+
+    let imageWidth = parseInt(url.searchParams.get('width'))
+    let imageHeight = parseInt(url.searchParams.get('height'))
+    try {
+        const img = await loadImage(imageSrc)
+        const r = img.width() / img.height()
+        if ((Number.isNaN(imageWidth) || imageWidth <= 0) && (Number.isNaN(imageHeight) || imageHeight <= 0)) {
+            imageWidth = r * defaultHeight
+            imageHeight = defaultHeight
+        } else if (!Number.isNaN(imageWidth) && imageWidth > 0) {
+            imageHeight = imageWidth / r
+        } else if (!Number.isNaN(imageHeight) && imageHeight > 0) {
+            imageWidth = r * imageHeight
+        }
+        ctx.drawImage(
+            img,
+            (canvasSize[0] - imageWidth) / 2,
+            (canvasSize[1] - imageHeight) / 2 - (text ? spacing / 2 : 0),
+            imageWidth,
+            imageHeight
+        )
+    } catch (e) {
+        event.respondWith(new Response(
+            `invalid image: ${imageSrc}`,
+            {
+                status: 400,
+                headers: { 'content-type': 'plain/text' },
+            }
+        ))
+        return
+    }
+
+    if (text) {
+        const fontSize = parseInt(url.searchParams.get('font-size')) || defaultFontSize
+        ctx.font = `italic bold ${fontSize}px Helvetica`
+        ctx.fillStyle = '#111111'
+
+        const t = ctx.measureText(text);
+        ctx.fillText(text, (canvasSize[0] - t.width) / 2, (canvasSize[1] + imageHeight + spacing) / 2, canvasSize[0] - spacing * 2)
+    }
+
+    event.respondWith(new Response(
+        canvas.toBuffer(),
+        {
+            headers: { 'content-type': 'image/png' },
+        }
+    ))
+})
