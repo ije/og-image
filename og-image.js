29b0096c5131fddd0f1e7986b292db9a4cd3a511
Je Xia <xiawenjie.798@bytedance.com>
2021-03-30 23:45:33 +0800

    chore: update deno-canvas url

--- og-image.js
+++ og-image.js
@@ -1,4 +1,4 @@
-import Canvas, { loadImage } from 'https://raw.githubusercontent.com/postui/og-image/main/deno-canvas/mod.js'
+import Canvas, { loadImage } from 'https://raw.githubusercontent.com/postui/og-image/main/deno-canvas-2/mod.js'
 
 addEventListener('fetch', async (event) => {
     const canvasSize = [2048, 1170]
